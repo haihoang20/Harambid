8cad12007c69c10e1c28ceda4c6b9966223599bd
Jastine94 <Jastine@dhcp-128-189-214-46.ubcsecure.wireless.ubc.ca>
2017-03-18 16:04:50 -0700

    stub from item and member controllers

--- controllers/api/itemController.js
+++ controllers/api/itemController.js
@@ -1,4 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
+router.get('/filterItem', function(req, res){
+    // get item from database
+}
+
+router.post('/addItem', function(req, res){
+    String imgSource = req.imgSource;
+    String name = req.name;
+    var maxPrice;
+    var minPrice;
+    var duration;
+    var categories [];
+    var description;
+    var shippingCost;
+    var isAuthenticated;
+    // add item to database
+}
+
+
+
 module.exports = router;
--- controllers/api/memberController.js
+++ controllers/api/memberController.js
@@ -1,4 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+router.get('/getMember', function(req, res){
+    // get member from database
+}
+
+
+
 module.exports = router;
