053168fc121a775be861e14cacb31853e28be5ca
xlrn <ncl.yim@gmail.com>
2017-03-18 21:53:27 -0700

    Fixed some lines

--- db/db_queries.js
+++ db/db_queries.js
@@ -1,7 +1,6 @@
 var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('./Harambid.db');
 
-
 function addNewItem(memberId, name, pictureURL, startTime, duration, minPrice, desc, avail, numViews, categories) {
 	var stmt = db.prepare("INSERT INTO item (MemberId, Name, Pictures, StartTime, Duration, StartPrice, MinPrice, Description, Categories, Availibility, Num_Views) VALUES (?,?,?,?,?,?,?,?,?,?,?)");
 	stmt.run(memberId, name, pictureURL, startTime, duration, minPrice, desc, categories, avail, numViews);
@@ -29,4 +28,7 @@ function setItemAvailibility(id, avail) {
 	db.close;
 }
 
-module.exports = {addNewItem : addNewItem, getItemsBasedOnCategory : getItemsBasedOnCategory, setItemAvailibility : setItemAvailibility};
+module.exports = {
+	addNewItem : addNewItem,
+	getItemsBasedOnCategory : getItemsBasedOnCategory,
+	setItemAvailibility : setItemAvailibility};
